ea2bb0e757cabc986700ac24659eacf768b007f4
Izahe <izahe@udel.edu>
2021-09-16 19:02:09 -0400

    Updated NavBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import axios from 'axios'
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
 
-class App extends Component {
-  render (){
+function App(): JSX.Element {
+  const [pokemon, setPokemon] = useState<string>("pikachu");
+  const getPokemon = async () => {
+    const toArray = [];
+    try{
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const res = await axios.get(url);
+      console.log(res);
+    } catch(error){
+      console.log(error)
+    }
+  }
+
     return (
     <div className="App">
       <NavBar />
       <div className="container">
         <Dashboard />
-        </div>
+        </div>  
     </div>
     );
-  }
 }
 
 export default App;
