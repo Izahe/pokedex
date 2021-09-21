7eba862e9ca48196a194ca196c86c337155a6a1f
Izahe <izahe@udel.edu>
2021-09-21 18:25:20 -0400

    Cleanup, Fixed Pokeball, Added support for multiple cards

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-//import styled from 'styled-components';
+import React, { Component } from 'react';
+import pokeball from './pokeball.png'
+//import styled from 'styled-componesnts';
 
 export default class NavBar extends Component {
     render(){
@@ -7,7 +8,7 @@ export default class NavBar extends Component {
             <div>
                 <div>
                 <nav className = "navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                    <img src="../../pokeball.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
+                    <img src={pokeball} width="30" height="30" className="d-inline-block align-top" alt=""></img>
                     <a href = "" className="navbar-brand"> Team Builder </a>
                     
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,15 +1,20 @@
+import red_x from './red_x.png'
 
-export const PokemonCard = (props: {name: string, image: string}): JSX.Element => {
+export const PokemonCard = ({list}: any): JSX.Element => {
     return (
-        <div className = 'col-md-3 col-sm-6 mb-5'>
-              <div className="card">
-                   <div className="card-header">
-                    <h1>{props.name}</h1>
-                   </div>
-                  <div className = "card-image">
-                    <img src = {props.image} alt = {props.name} width = "300" height = "300"></img>
-                  </div> 
-             </div>
+      <>
+        {list.map((card: {name: string, image: string}) => (
+            <div className = 'col-md-2 col-sm-6 mb-6'>
+            <div className="card">
+                <div className="card-header">
+                  <h1>{card.name}<button type = "button" className="btn btn-light"><img src = {red_x} height = "25px"></img></button></h1>
+                </div>
+                <div className = "card-image">
+                  <img src = {card.image} alt = {card.name} width = "150" height = "150"></img>
+                </div> 
           </div>
+        </div>
+        ))}
+      </>
     )
 }
