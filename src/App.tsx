c14ee6e8f28c44604b36c5e1993067a7d86c293e
Izahe <izahe@udel.edu>
2021-09-23 12:36:31 -0400

    site now starts with 0 cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,37 @@ import axios from 'axios'
 import NavBar from './components/layout/NavBar';
 import { PokemonCard } from './components/pokemon/PokemonCard';
 
+interface Pokemon {
+  name: string;
+  image: string;
+}
 function App(): JSX.Element {
   const [pokemon, setPokemon] = useState<string>("pikachu");
-  const [pokemonList, setPokemonList] = useState([
-    {
-      name: "pikachu",
-      image: "a"
-    }
-  ])
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([])
   const getPokemon = async () => {
     try{
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-      const res = await axios.get(url);
-      let pokemon_name: string = res.data.name;
-      //Making the first letter capitalized
-      pokemon_name = pokemon_name.substring(0,1).toUpperCase() + pokemon_name.substring(1);
-      const pokemon_image: string = res.data.sprites.front_default;
-      const new_pokemon = {name: pokemon_name, image: pokemon_image}; 
-      setPokemonList([...pokemonList, new_pokemon])
-      console.log(res);
+      if (pokemonList.length == 6){
+        alert("A team can only have 6 pokemon!")
+      }
+      else{
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        const res = await axios.get(url);
+        let pokemon_name: string = res.data.name;
+        //Making the first letter capitalized
+        pokemon_name = pokemon_name.substring(0,1).toUpperCase() + pokemon_name.substring(1);
+        const pokemon_image: string = res.data.sprites.front_default;
+        const new_pokemon = {name: pokemon_name, image: pokemon_image}; 
+        setPokemonList([...pokemonList, new_pokemon]);
+        console.log(res);
+      }
     }
     catch(error){
-      console.log(error)
+      console.log(error);
     }
   };
 
   const handleChange = (input: any) => {
-    setPokemon(input.target.value.toLowerCase())
+    setPokemon(input.target.value.toLowerCase());
   }
 
   const handleSubmit = (mon: any) => {
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -7,7 +7,7 @@ export const PokemonCard = ({list}: any): JSX.Element => {
             <div className = 'col-md-2 col-sm-6 mb-6'>
             <div className="card">
                 <div className="card-header">
-                  <h1>{card.name}<button type = "button" className="btn btn-light"><img src = {red_x} height = "25px"></img></button></h1>
+                  <h1>{card.name}<button type = "button" className="btn btn-light" ><img src = {red_x} height = "25px"></img></button></h1>
                 </div>
                 <div className = "card-image">
                   <img src = {card.image} alt = {card.name} width = "150" height = "150"></img>
